48dc364e812233f911c7cd906b8ade5076327e9e
tumieuyeu <tumieuyeu@gmail.com>
2020-08-14 15:08:28 +0700

    third commit

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -7,13 +7,36 @@ for(var i = 0; i < updateBtns.length; i++){
         console.log('productId: ', productId, 'action: ', action)
 
         console.log('User: ', user)
-        if(user === 'AnonymousUser'){
-            console.log('User is not authenticated')
+        if(user == 'AnonymousUser'){
+            addCookieItem(productId, action)
         }
         else{
             updateUserOrder(productId, action)
         }
     })   
+}
+
+
+function addCookieItem(productId, action){
+    console.log('Not logged in...')
+    if(action == 'add'){
+        if(cart[productId] == undefined){
+            cart[productId] = {'quantity': 1}
+        }
+        else{
+            cart[productId]['quantity'] += 1
+        }
+    }
+    if(action == 'remove'){
+        cart[productId]['quantity'] -= 1
+        if(cart[productId]['quantity'] <= 0){
+            console.log('Item should be deleted')
+            delete cart[productId];
+        }
+    }
+    console.log('Cart: ', cart)
+    document.cookie = 'cart = ' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
 }
 
 function updateUserOrder(productId, action){
@@ -32,5 +55,6 @@ function updateUserOrder(productId, action){
     })
     .then((data)=>{
         console.log('data: ', data)
+        location.reload()
     });
 }
--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -11,7 +11,7 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
-    price = models.FloatField(default=0.0)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=False)
     image = models.ImageField(null=True, blank=True)
     def __str__(self):
@@ -30,7 +30,15 @@ class Order(models.Model):
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=200, null=True)
     def __str__(self):
-        return str(self.transaction_id)
+        return str(self.id)
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
     @property
     def get_cart_total(self):
         orderitems = self.orderitem_set.all()
@@ -47,6 +55,8 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return 'order item of order ' + str(self.order.id)
     @property
     def get_total(self):
         total = self.product.price * self.quantity
@@ -54,7 +64,7 @@ class OrderItem(models.Model):
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
-    Order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     address = models.CharField(max_length=200, null=True)
     city = models.CharField(max_length=200, null=True)
     state = models.CharField(max_length=200, null=True)
